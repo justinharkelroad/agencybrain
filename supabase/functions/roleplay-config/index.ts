350a550dda56bedd5f620c2e4650a38cc497020d
justinharkelroad <justin@triumphfw.com>
2026-02-05 13:11:59 -0500

    Fix: Standardize ALL edge functions to supabase-js@2.49.1

--- supabase/functions/roleplay-config/index.ts
+++ supabase/functions/roleplay-config/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { supaFromReq } from "../_shared/client.ts";
 import { corsHeaders } from "../_shared/cors.ts";
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.53.0';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.49.1';
 
 serve(async (req) => {
   if (req.method === 'OPTIONS') {
